0db107ecef4ccbd11968b2ce728819bd7075461d
Jatin Foujdar <jatin321001@gmail.com>
2025-11-20 22:05:48 +0530

    Initial Commit

---
+++ Anchor/AnchorApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnchorApp.swift
+//  Anchor
+//
+//  Created by jatin foujdar on 20/11/25.
+//
+
+import SwiftUI
+
+@main
+struct AnchorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
