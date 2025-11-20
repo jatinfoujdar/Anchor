9254f4c5aee8ea31cb7c746610ba006744e166a9
Jatin Foujdar <jatin321001@gmail.com>
2025-11-21 01:52:31 +0530

    daily viewmodel commit

--- Anchor/ContentView.swift
+++ Anchor/ContentView.swift
@@ -8,36 +8,14 @@
 import SwiftUI
 
 struct ContentView: View {
-//    @ObservedObject var viewModel: NeoPopHeroSectionViewModel
+    @StateObject private var viewModel = ContentViewModel()
     
     var body: some View {
-        NeoPopCard(
-            surface: .neoPrimarySurface,
-            highlight: .neoHighlight,
-            edge: .neoEdge,
-            cornerRadius: 32,
-            edgeDepth: 12,
-            floating: true
-        ) {
-            VStack(alignment: .leading, spacing: 20) {
-                HStack(alignment: .center) {
-                    Text("viewModel: viewModel.tag")
-                    Spacer()
-                    Text("viewModel.dueDescription")
-                        .font(.system(size: 12))
-                        .textCase(.uppercase)
-                        .foregroundStyle(Color.white.opacity(0.6))
-                }
-                .padding()
-            }
-        }
-        .padding(.top, 20)
-        .padding(.horizontal, 20)
-        .padding(.bottom, 32)
+        NeoPopNavBar(viewModel: viewModel.navBar)
     }
 }
 
 #Preview {
     ContentView()
-        
+        .preferredColorScheme(.dark)
 }
